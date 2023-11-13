6626b27319855b90ca0e0e093a3d174132aa22a1
unknown <denkata142@gmail.com>
2023-11-13 23:48:34 +0200

    Fix Flake8

--- src/app.py
+++ src/app.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 def hello_world():
     return "Hello! I am a Flask application running on {}".format(socket.gethostname())
 
-
 if __name__ == '__main__':
     # Note the extra host argument. If we didn't have it, our Flask app
     # would only respond to requests from inside our container
